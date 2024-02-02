d6c9f9e104e0467caa174f9ceefae4da280408c3
Oli Chase <olichase91@gmail.com>
2024-02-01 16:25:17 -0800

    Add new feature

--- run.py
+++ run.py
@@ -2,6 +2,11 @@ import subprocess
 import random
 import time
 from mimesis import Generic, Code
+from faker import Faker
+
+faker = Faker()
+
+features = []
 
 def flip_coin():
     rand = random.randint(0, 1)
@@ -10,8 +15,14 @@ def flip_coin():
     else:
         return False
 
-def generate_commit_message():
-    msg = ''
+def update_feature_list():
+    path = './Super_Awesome_App'
+    command = ['ls', path]
+    result = subprocess.run(command, capture_output=True, text=True)
+    if result.returncode == 0:
+        features = result.stdout.splitlines()
+        print(features)
+    
     
 def generate_code(num_lines=5):
     code = Code()
@@ -23,7 +34,8 @@ def take_break():
     
 def new_feature():
     print("I'm adding a new feature!")
-    command = 'touch ./Super_Awesome_App/feature' + str(random.randint(0, 1000000)) + '.py'
+    name = 'feature' + str(random.randint(0, 1000000)) + '.py'
+    command = 'touch ./Super_Awesome_App/' + name
     try:
         subprocess.run(command, shell=True)
         subprocess.run(['git', 'add', '.'])
@@ -32,12 +44,23 @@ def new_feature():
     except:
         print("Oh, that didn't work...")
     time.sleep(random.randint(5, 30))
+    
+def update_readme():
+    print("Better update the README!")
+    flip = flip_coin()
+    if flip:
+        with open('./README.md', 'a') as f:
+            f.write('\n\n' + faker.sentence())
+    
+    subprocess.run(['git', 'add', '.'])
+    subprocess.run(['git', 'commit', '-am', 'Update README'])
+    subprocess.run(['git', 'push'])
 
 
 
-# def work():
-#     print("I'm working!")
-#     if flip_coin():
+def work():
+    time.sleep(random.randint(5, 30))
     
     
+# update_feature_list()
 new_feature()
